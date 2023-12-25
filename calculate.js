515644aaba37b546a2573d331cf96aa182ef0171
Konain1 <kaunainkasab@gmail.com>
2023-12-25 09:14:27 +0530

    os node.js understood

---
+++ calculate.js
@@ -0,0 +1,12 @@
+
+let num1 = 10;
+let num2 = 20;
+
+function add(num1,num2){
+    console.log(num1+num2 + " add")
+}
+function sub(a,b){
+    console.log(a-b + " sub")
+}
+add(num1,num2)
+sub(num1,num2)
---
+++ os.js
@@ -0,0 +1,8 @@
+
+const os = require('os')
+
+// console.log((os.uptime().toFixed()) % 1000)
+
+const user = os.userInfo()
+
+console.log(os.type())
